3197735193b229ccc0ac388fab786c9b82e6e65a
Luke <lukeandshuo@sina.com>
2017-01-01 12:40:09 -0800

    add IR image registration function in retrieve metric module

--- arf_parser.py
+++ arf_parser.py
@@ -6,7 +6,7 @@ import numpy as np
 import time
 from skimage import img_as_ubyte
 from skimage import exposure
-Dir = "sample_data/Imagery/IR/"
+Dir = "sample_data/Imagery/Visible/"
 VideoDir = Dir+"videos/"
 fileList = os.listdir(VideoDir)
 start = time.clock()
