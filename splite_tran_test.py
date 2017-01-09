e7f60d27be98a37d25807a6d19a9311eb33ec397
luke <lukeandshuo@sina.com>
2017-01-09 10:27:58 -0800

    splite dataset into 5:3

--- splite_tran_test.py
+++ splite_tran_test.py
@@ -4,13 +4,21 @@ from pandas import Series,DataFrame
 import pandas  as pd
 import numpy as np
 import time
-ImgType = "IR_Reg"
-Annotations_dir = op.join("sample_data/Annotations",ImgType)
-if not os.path.exists(Annotations_dir):
-    os.makedirs(Annotations_dir)
-Train_Test_dir = op.join("sample_data/Train_Test",ImgType)
-if not os.path.exists(Train_Test_dir):
-    os.makedirs(Train_Test_dir)
+IRImgType = "IR_Reg"
+VImgType = "Visible"
+IR_Annotations_dir = op.join("sample_data/Annotations",IRImgType)
+if not os.path.exists(IR_Annotations_dir):
+    os.makedirs(IR_Annotations_dir)
+V_Annotations_dir = op.join("sample_data/Annotations",VImgType)
+if not os.path.exists(V_Annotations_dir):
+    os.makedirs(V_Annotations_dir)
+
+IR_list_dir = op.join("sample_data/Train_Test",IRImgType)
+if not os.path.exists(IR_list_dir):
+    os.makedirs(IR_list_dir)
+V_list_dir = op.join("sample_data/Train_Test",VImgType)
+if not os.path.exists(V_list_dir):
+    os.makedirs(V_list_dir)
 
 GT_file = op.join("sample_data/GroundTruth","Visible","FullList.csv")
 
@@ -19,23 +27,34 @@ if not os.path.isfile(GT_file):
 else:
     df = pd.read_csv(GT_file,na_values=['-1',' '])
     clean_df = df[pd.notnull(df['Match'])]
-    train_df = clean_df[(clean_df["TgtType"]!="ZSU23") & (clean_df["TgtType"]!="2S3")]
+    train_df = clean_df[(clean_df["TgtType"]!="ZSU23") & (clean_df["TgtType"]!="2S3")& (clean_df["TgtType"]!="SUV")]
     train_df = train_df[train_df["Frame"]%5 == 0] # skip 5 frame
-    test_df = clean_df[(clean_df["TgtType"]=="ZSU23") | (clean_df["TgtType"]=="2S3")]
+    test_df = clean_df[(clean_df["TgtType"]=="ZSU23") | (clean_df["TgtType"]=="2S3")| (clean_df["TgtType"]=="SUV")]
     test_df = test_df[test_df["Frame"]%5==0]
     train_df = train_df.reindex(np.random.permutation(train_df.index))
     #test_df = test_df.reindex(np.random.permutation(test_df.index))
-    train_image = train_df["Match"]
+    print len(test_df['TgtType'])
+    print len(train_df['TgtType'])
+    IR_train_image = train_df["Match"]
+    V_train_image = train_df['ImageName']
     train_box= DataFrame({"X1":train_df['UpperLeft'],"Y1":train_df["UpperTop"],"X2":train_df["UpperLeft"]
                      +train_df["BBoxW"],"Y2":train_df["UpperTop"]+train_df["BBoxH"]},columns=["X1","Y1","X2","Y2"])
-    test_image = test_df["Match"]
+    IR_test_image = test_df["Match"]
+    V_test_image = test_df['ImageName']
     test_box= DataFrame({"X1":test_df['UpperLeft'],"Y1":test_df["UpperTop"],"X2":test_df["UpperLeft"]
                      +test_df["BBoxW"],"Y2":test_df["UpperTop"]+test_df["BBoxH"]},columns=["X1","Y1","X2","Y2"])
-    train_image.to_csv(op.join(Train_Test_dir,"train.txt"),index=False,header=False)
-    train_box.to_csv(op.join(Annotations_dir,"train.txt"),index=False,header=False)
 
-    test_image.to_csv(op.join(Train_Test_dir,"test.txt"),index=False,header=False)
-    test_box.to_csv(op.join(Annotations_dir,"test.txt"),index=False,header=False)
+    IR_train_image.to_csv(op.join(IR_list_dir,"train.txt"),index=False,header=False)
+    train_box.to_csv(op.join(IR_Annotations_dir,"train.txt"),index=False,header=False)
+
+    IR_test_image.to_csv(op.join(IR_list_dir,"test.txt"),index=False,header=False)
+    test_box.to_csv(op.join(IR_Annotations_dir,"test.txt"),index=False,header=False)
+
+    V_train_image.to_csv(op.join(V_list_dir,"train.txt"),index=False,header=False)
+    train_box.to_csv(op.join(V_Annotations_dir,"train.txt"),index=False,header=False)
+
+    V_test_image.to_csv(op.join(V_list_dir,"test.txt"),index=False,header=False)
+    test_box.to_csv(op.join(V_Annotations_dir,"test.txt"),index=False,header=False)
 
     # cls_df = clean_df["TgtType"]
     # for i in range(len(cls_df)):
